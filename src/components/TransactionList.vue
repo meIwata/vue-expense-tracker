0b5b4e79504ab43d4ad0dbac1350b4c6aadb2b20
waleilulu <waleilulu@gmail.com>
2025-01-24 20:37:08 +0800

    Add new files

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,24 +1,33 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0? 'minus' : 'plus'">
+        <li v-for="transaction in transactions" :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'">
             {{ transaction.text }} <span>${{ transaction.amount }}</span>
             <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
 
-<script>
-    export default {
-        data(){
-            return{
-                transactions: [
-                    { id: 1, text: 'Flower', amount: -19.99 },
-                    { id: 2, text: 'Salary', amount: 300 },
-                    { id: 3, text: 'Book', amount: -27.99 },
-                    { id: 2, text: 'Cash dividend', amount: 90 },
-                ],
-            };
-        },
-    };
+<!-- <script>
+export default {
+    data() {
+        return {
+            transactions: [
+                { id: 1, text: 'Flower', amount: -19.99 },
+                { id: 2, text: 'Salary', amount: 300 },
+                { id: 3, text: 'Book', amount: -27.99 },
+                { id: 2, text: 'Cash dividend', amount: 90 },
+            ],
+        };
+    },
+};
+</script> -->
+<script setup>
+const transactions = [
+    { id: 1, text: 'Flower', amount: -19.99 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -27.99 },
+    { id: 2, text: 'Cash dividend', amount: 90 },
+];
 </script>
