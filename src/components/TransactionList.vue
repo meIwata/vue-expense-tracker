145ca04502ebcc2ea082ff57312c04a62f4c9a09
waleilulu <waleilulu@gmail.com>
2025-01-24 20:25:27 +0800

    Add new files

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,6 +1,10 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
+        <li v-for="transaction in transactions":key="transaction.id">
+            Cash <span>-$400</span>
+            <button class="delete-btn">x</button>
+        </li>
         <!-- <li class="minus">
             Cash <span>-$400</span><button class="delete-btn">x</button>
         </li>
@@ -9,3 +13,18 @@
         </li> -->
     </ul>
 </template>
+
+<script>
+    export default {
+        data(){
+            return{
+                transactions: [
+                    { id: 1, text: 'Flower', amount: -19.99 },
+                    { id: 2, text: 'Salary', amount: 300 },
+                    { id: 3, text: 'Book', amount: -27.99 },
+                    { id: 2, text: 'Cash dividend', amount: 90 },
+                ],
+            };
+        },
+    };
+</script>
