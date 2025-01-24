cfac373f2402a7dd23a5ef1d37bc9eade28a095c
waleilulu <waleilulu@gmail.com>
2025-01-24 20:31:48 +0800

    Add new files

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,16 +1,10 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li v-for="transaction in transactions":key="transaction.id">
-            Cash <span>-$400</span>
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0? 'minus' : 'plus'">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span>
             <button class="delete-btn">x</button>
         </li>
-        <!-- <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Paycheck <span>$800</span><button class="delete-btn">x</button>
-        </li> -->
     </ul>
 </template>
 
